44b190420f5559168a768d4e77d9a784edb6245b
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-06-10 20:13:00 -0300

    Add Aula_04_If_Else

---
+++ Aula_04_If_Else.py
@@ -0,0 +1,18 @@
+## Execircio 1
+#n1 = int(input("Digite um número: "))
+#n2 = float(input("Digite a nota 2: "))
+
+# if n1 % 2 == 0:
+#     print(f'\nNumero par')
+# else:
+#     print(f'\nNúmero impar')
+
+## Exercio 2
+
+num1 = int(input("Digite um número: "))
+if num1 == 0:
+    print(f'Número neutro')
+elif num1 > 0:
+    print(f'\nNumero positivo')
+else:
+    print(f'\nNúmero negativo')
