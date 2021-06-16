6b70dd40bf349adfea66544a1dc5d33d13c914d5
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-06-15 22:44:56 -0300

    Programa que ler 7 idades e diz quantas pessoas tem menos de 18 anos e quantas tem 18 ou mais

---
+++ aulas/aula_07/Exercicio_03.py
@@ -0,0 +1,26 @@
+# 03 - Crie um programa que leia o ano de nascimento de sete pessoas. No final,
+# mostre quantas pessoas ainda não atingiram a maioridade e quantas já são
+# maiores.
+
+from datetime import date
+from random import randint
+
+birthyear = {}
+age = {}
+curYear = date.today()
+c = 0
+underAge = ofAge = 0
+
+while c < 7:
+    birthyear[c] = randint(1920, curYear.year)
+    age[c] = curYear.year - birthyear[c]
+    print(f'Idade pessoa {c+1}: {age[c]}')
+   
+    if int(age[c]) < 18:
+        underAge += 1
+    else:
+        ofAge += 1
+
+    c += 1
+
+print(f'\n\nPessoas com menos de 18 anos: {underAge}\nPessoas com 18 anos ou mais: {ofAge}')
